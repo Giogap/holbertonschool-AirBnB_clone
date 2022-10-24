2abba14e413bebbf7e00b38c0a79ae92d09b9aa7
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 <giogap@gmail.com>
2022-10-24 15:31:45 -0500

    upload base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
+""""""
 
+import uuid
+import datetime
 
 class BaseModel:
-    pass
+
+    def __init__(self, id, create_at, update_at):
+        self.id = str(uuid.uuid4())
+        self.create_at = datetime.now()
+        self.update_at = datetime.now()
+
+
+    def __str__(self):
+        return "[BaseModel] {} {}".format(self.id, self.__dict__)
+
+    
+    def save(self):
+        pass
+
+
