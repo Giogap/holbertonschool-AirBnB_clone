26121ef71b4dbfc8b1206b92ae17fb21ad0462c7
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-24 19:23:39 -0500

    update-PP8

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+"""
+Test BaseModel
+"""
+
+
 from models.base_model import BaseModel
+import unittest
+
+class TestsBaseModel(unittest.TestCase):
+    """ Test BaseModel """
+    def testBaseMdl(self):
+        pass
