490bf2aeb5a9d0bad69a356c8e9cc7ce4d80bcf9
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-24 19:41:59 -0500

    update-PP8

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,8 +6,11 @@ Test BaseModel
 
 from models.base_model import BaseModel
 import unittest
+from datetime import datetime
+
 
 class TestsBaseModel(unittest.TestCase):
     """ Test BaseModel """
-    def testBaseMdl(self):
-        pass
+    def testBaseMdlSave(self):
+        save = datetime.now()
+        self.assertEqual(save, datetime.now())
