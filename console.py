e6b6444e078740ddaec1327f467bb4d76c9ff3a2
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-28 11:15:41 -0500

    console

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+console that contains the entry point of the command interpreter
+"""
+import cmd, sys
+
+
+class HBNBCommand(cmd.Cmd):
+    """ clas HBNB cmd """
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
