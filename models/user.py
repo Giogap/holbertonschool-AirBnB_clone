0316cb02965c6ee2025cd26b5f4fdfd4d0adf008
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 <giogap@gmail.com>
2022-10-29 17:30:24 -0500

    upload user

--- models/user.py
+++ models/user.py
@@ -6,9 +6,7 @@ from models.base_model import BaseModel
 
 class User:
     """"""
-
-    def __init__(self):
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
