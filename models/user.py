ab9a2bf69e889e93a81ff2a07707bec03e7d460c
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-29 17:48:53 -0500

    update-user- class attributes

--- models/user.py
+++ models/user.py
@@ -7,9 +7,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """class User that inherits from BaseModel"""
-
-    def __init__(self):
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
