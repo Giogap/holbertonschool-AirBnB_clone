7f0efeec207a3383c3c840bdfac3fe5add750f3d
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-28 21:42:26 -0500

    update

--- console.py
+++ console.py
@@ -4,12 +4,20 @@ console that contains the entry point of the command interpreter
 """
 import cmd
 from models.base_model import BaseModel
+import models
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
 
 
 class HBNBCommand(cmd.Cmd):
     """ clas HBNB cmd """
     prompt = '(hbnb) '
-    filen = None
+    list_class = ["BaseModel", "State", "City", "Amenity",
+                 "Place", "Review", "User"]
 
     def do_create(self, arg):
         """ Creates a new instance of BaseModel, saves JSON file"""
@@ -22,7 +30,26 @@ class HBNBCommand(cmd.Cmd):
                 print(new_obj.id)
             except (NameError, SyntaxError):
                 print("** class doesn't exist **")
-                pass
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance
+        based on the class name and id
+        """
+        command = self.parseline(arg)[0]
+        arg = self.parseline(arg)[1]
+        if command is None:
+            print("** class name missing **")
+        elif command not in self.list_class:
+            print("** class doesn't exist **")
+        elif arg == "":
+            print("** instance id missing **")
+        else:
+            instance = models.storage.all().get(command + "." + arg)
+            if instance is None:
+                print("** no instance found **")
+            else:
+                print(instance)
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
