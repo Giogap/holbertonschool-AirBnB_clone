38c25e6fc25ac356b7de97f029e48e5125fee535
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 <giogap@gmail.com>
2022-10-29 19:20:50 -0500

    user

--- models/user.py
+++ models/user.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-"""class User"""
+"""class User that inherits from BaseModel"""
 
 
 from models.base_model import BaseModel
 
-<<<<<<< HEAD
-class User:
-    """"""
-=======
-
 class User(BaseModel):
-    """class User that inherits from BaseModel"""
->>>>>>> 06881f103fb1d9017cc72e0c6493ce590c213c89
+    """"""
     email = ""
     password = ""
     first_name = ""
