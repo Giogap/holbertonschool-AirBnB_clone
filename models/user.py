b9c16d4944b52d945da52c93b0d7e6741c4140f3
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 <giogap@gmail.com>
2022-10-29 19:36:11 -0500

    update user

--- models/user.py
+++ models/user.py
@@ -4,8 +4,9 @@
 
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
-    """"""
+    """Class user that inherits from BaseModel"""
     email = ""
     password = ""
     first_name = ""
