f4bf75d71e9702d12e6d1c624c7e7641ce2e50f2
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-30 07:53:42 -0500

    restore

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,77 @@
+
+#!/usr/bin/python3
+"""Test FileStorage"""
+
+
+import unittest
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+import models
+import os.path
+
+
+class TestFileStorage(unittest.TestCase):
+    """TestCase FileStorage"""
+    storage = FileStorage()
+    obj = storage.all()
+    def test_fileStorage(self):
+        self.assertIsNotNone(obj)
+        self.assertEqual(type(obj), dict)
+        self.assertIs(obj, storage._FileStorage__objects)
+
+    def test_fileStore_save(self):
+        """Test FileStore Save"""
+        storage = FileStorage()
+        base = BaseModel()
+        storage.new(base)
+        storage.save()
+        with open("file.json", "r", encoding='utf-8') as r:
+            file_Json = r.read()
+            self.assertTrue(f"BaseModel.{base.id}" in file_Json)
+            file_json = r.read()
+            self.assertTrue(f"BaseModel.{base.id}" in file_Json)
+
+    def test_file(self):
+        """test reload"""
+        storage = FileStorage()
+        obj = storage.all()
+        copy = obj.copy()
+        models.storage.reload()
+        copy2 = obj.copy()
+        self.assertEqual(len(copy), len(copy2))
+        storage.save()
+        self.assertIsInstance(storage._FileStorage__file_path, str)
+        self.assertIsInstance(storage._FileStorage__objects, dict)
+        self.assertTrue(os.path.exists("file.json"))
+
+    def test_reload(self):
+        """ Test Reload"""
+        storage = FileStorage()
+        base = BaseModel()
+        storage.save()
+        for obj in storage.all().values():
+            loaded = obj
+        self.assertEqual(base.to_dict()['id'], loaded.to_dict()['id'])
+
+    def test_reload_empty(self):
+        """ Doc """
+        storage = FileStorage()
+        with open('file.json', 'w') as f:
+            pass
+        with self.assertRaises(ValueError):
+            storage.reload()
+
+    def test_reload_none(self):
+        """ Doc """
+        storage = FileStorage()
+        self.assertEqual(storage.reload(), None)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
