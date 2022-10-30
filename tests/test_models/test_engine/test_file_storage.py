ee7df4d0232940e104312d676606ccb00ccd946c
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-30 00:28:30 -0500

    update-error-import-filestore

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -38,6 +38,24 @@ class TestFileStorage(unittest.TestCase):
         self.assertIsInstance(storage._FileStorage__objects, dict)
         self.assertTrue(os.path.exists("file.json"))
 
+    def test_reload(self):
+        """ Test Reload"""
+        storage.save()
+        for obj in storage.all().values():
+            loaded = obj
+        self.assertEqual(base.to_dict()['id'], loaded.to_dict()['id'])
+
+    def test_reload_empty(self):
+        """ Doc """
+        with open('file.json', 'w') as f:
+            pass
+        with self.assertRaises(ValueError):
+            storage.reload()
+
+    def test_reload_none(self):
+        """ Doc """
+        self.assertEqual(storage.reload(), None)
+
 
 storage = FileStorage()
 obj = storage.all()
