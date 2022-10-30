f41666b8344d62392b80b603a1e94f59e9a4aef1
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-30 08:27:36 -0500

    update-test-file

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -31,6 +31,19 @@ class TestFileStorage(unittest.TestCase):
             file_json = r.read()
             self.assertTrue(f"BaseModel.{base.id}" in file_Json)
 
+    def test_file(self):
+        """test reload"""
+        storage = FileStorage()
+        obj = storage.all()
+        copy = obj.copy()
+        models.storage.reload()
+        copy2 = obj.copy()
+        self.assertEqual(len(copy), len(copy2))
+        storage.save()
+        self.assertIsInstance(storage._FileStorage__file_path, str)
+        self.assertIsInstance(storage._FileStorage__objects, dict)
+        self.assertTrue(os.path.exists("file.json"))
+
     def test_reload(self):
         """ Test Reload"""
         storage = FileStorage()
@@ -39,19 +52,6 @@ class TestFileStorage(unittest.TestCase):
         for obj in storage.all().values():
             loaded = obj
         self.assertEqual(base.to_dict()['id'], loaded.to_dict()['id'])
-
-    def test_reload_empty(self):
-        """ Doc """
-        storage = FileStorage()
-        with open('file.json', 'w') as f:
-            pass
-        with self.assertRaises(ValueError):
-            storage.reload()
-
-    def test_reload_none(self):
-        """ Doc """
-        storage = FileStorage()
-        self.assertEqual(storage.reload(), None)
 
 
 if __name__ == '__main__':
