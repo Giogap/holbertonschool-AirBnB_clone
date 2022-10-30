8f60bb60a5b8a3641814c2f9fbe115ad2ce064eb
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-10-30 00:11:22 -0500

    update-tes-fileServer

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,7 +1,8 @@
+
 #!/usr/bin/python3
 """Test FileStorage"""
 
-from ast import Store
+
 import unittest
 from models.engine.file_storage import FileStorage
 import models
@@ -11,18 +12,13 @@ import os
 
 class TestFileStorage(unittest.TestCase):
     """TestCase FileStorage"""
-
     def test_fileStorage(self):
-        storage = FileStorage()
-        obj = storage.all()
         self.assertIsNotNone(obj)
         self.assertEqual(type(obj), dict)
         self.assertIs(obj, storage._FileStorage__objects)
 
     def test_fileStore_save(self):
         """Test FileStore Save"""
-        storage = FileStorage()
-        base = BaseModel()
         storage.new(base)
         storage.save()
         with open("file.json", "r", encoding='utf-8') as r:
@@ -33,8 +29,6 @@ class TestFileStorage(unittest.TestCase):
 
     def test_file(self):
         """test reload"""
-        storage = FileStorage()
-        obj = storage.all()
         copy = obj.copy()
         models.storage.reload()
         copy2 = obj.copy()
@@ -45,24 +39,28 @@ class TestFileStorage(unittest.TestCase):
         self.assertTrue(os.path.exists("file.json"))
 
     def test_reload(self):
-        """ >Test Reload """
-        Storage = FileStorage()
-        Storage.reload()
-        self.assertIsNotNone(Storage.all())
-        self.assertIs(Storage.all(), Storage.all())
-
-    def test_save_reload(self):
-        """ checking save and reload method """
-        storage = FileStorage()
-        base = BaseModel()
-        base.name = "Betty"
-        bid = base.id
-        storage.new(base)
+        """ Test Reload"""
         storage.save()
         storage.reload()
-        self.assertIsNotNone(
-            storage.all()[base.__class__.__name__ + "." + bid])
+        for obj in storage.all().values():
+            loaded = obj
+        self.assertEqual(base.to_dict()['id'], loaded.to_dict()['id'])
 
+    def test_reload_empty(self):
+        """ Doc """
+        with open('file.json', 'w') as f:
+            pass
+        with self.assertRaises(ValueError):
+            storage.reload()
+
+    def test_reload_none(self):
+        """ Doc """
+        self.assertEqual(storage.reload(), None)
+
+
+storage = FileStorage()
+obj = storage.all()
+base = BaseModel()
 
 if __name__ == '__main__':
     unittest.main()
